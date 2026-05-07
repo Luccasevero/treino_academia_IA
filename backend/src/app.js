e5490492367e145f257da88afa2f15e8d1871def
Luccasevero <Lucca.severo@hotmail.com>
2026-05-07 20:15:34 -0300

    Ajuste na ordem do dotenv e conexao db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import pool from "./database/db.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,12 +2,17 @@ import pkg from "pg";
 const { Pool } = pkg;
 import dotenv from "dotenv";
 
-dotenv.config(); // Garante que as variáveis de ambiente sejam carregadas
+dotenv.config(); 
+
+// Se DATABASE_URL estiver vazia, o código vai avisar no log em vez de tentar conectar no localhost
+if (!process.env.DATABASE_URL) {
+  console.error("❌ ERRO: A variável DATABASE_URL não foi detectada!");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { 
-    rejectUnauthorized: false // Isso é obrigatório para bancos na nuvem como o Neon
+    rejectUnauthorized: false 
   }
 });
 
