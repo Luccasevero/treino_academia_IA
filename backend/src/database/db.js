f728942933684aabfc45387ee3de2474ab2e1a65
Luccasevero <Lucca.severo@hotmail.com>
2026-05-07 19:34:03 -0300

    Backend conectado ao Neon com sucesso

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,12 +1,14 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import dotenv from "dotenv";
+
+dotenv.config(); // Garante que as variáveis de ambiente sejam carregadas
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "treino_db",
-    password: "23022006",
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: { 
+    rejectUnauthorized: false // Isso é obrigatório para bancos na nuvem como o Neon
+  }
 });
 
 export default pool;
